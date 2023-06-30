6ed57228c0a59562136147f69d4a210ef9a0fdab
SelimGüney <slmgny333@gmail.com>
2023-06-30 20:13:44 +0300

    Header Finished

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,25 +2,42 @@ import Nav from "react-bootstrap/Nav"
 
 const Header = () => {
   return (
-    <header className="header">
-      <Nav
-        variant="pills"
-        defaultActiveKey="/home"
-        className="just-content-end gap-2"
-      >
-        <Nav.Item>
-          <Nav.Link href="/home">Anasayfa</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link eventKey="link-1">Yazılarım</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link eventKey="link-2">Hakkımda</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link eventKey="link-3">Ekip ve Destek</Nav.Link>
-        </Nav.Item>
-      </Nav>
+    <header className="header d-flex p-2 align-items-center justify-content-between">
+      <div className="logo h2 p-4">
+        <span className="text-white">Atalay Ferit</span>
+      </div>
+      <div className="nav">
+        <Nav
+          variant="pills"
+          defaultActiveKey="/home"
+          className="just-content-end gap-2"
+        >
+          <Nav.Item>
+            <Nav.Link href="/home" target="_blank" className="text-white">
+              Anasayfa
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link
+              href="https://aferit.medium.com"
+              target="_blank"
+              className="text-white"
+            >
+              Yazılarım
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link href="#" target="_blank" className="text-white">
+              Hakkımda
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link href="#" target="_blank" className="text-white">
+              Ekip ve Destek
+            </Nav.Link>
+          </Nav.Item>
+        </Nav>
+      </div>
     </header>
   )
 }
